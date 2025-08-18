8809b7707fbe5051530b4aca443642073b44b2a8
baek_do_hyeon <ehgus6666@naver.com>
2025-08-18 14:19:27 +0900

    first commit

---
+++ src/test/java/org/example/newspeedproject/NewspeedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newspeedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspeedProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
