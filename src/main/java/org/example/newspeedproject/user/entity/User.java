6b9808a9eed163041686e7d8082dd7550ac65bd1
dbdnsdb123 <dbdnsdb123@naver.com>
2025-08-19 11:53:13 +0900

    feat: add user entity

--- src/main/java/org/example/newspeedproject/user/entity/User.java
+++ src/main/java/org/example/newspeedproject/user/entity/User.java
@@ -1,4 +1,37 @@
 package org.example.newspeedproject.user.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@RequiredArgsConstructor
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //pk, autoincrement
+
+    @Column(length = 4)
+    private String username;
+
+    @Column(length = 20)
+    private String email;
+
+    @Column(length = 100)
+    private String password;
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdDateAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedDateAt;
 }
