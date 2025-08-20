57bed46678dd08da33c6d90e484ed36fcac3a658
dbdnsdb123 <dbdnsdb123@naver.com>
2025-08-20 16:16:34 +0900

    ref: change User ProfileChangeRequestDto

--- src/main/java/org/example/newspeedproject/user/dto/ProfileChangeRequestDto.java
+++ src/main/java/org/example/newspeedproject/user/dto/ProfileChangeRequestDto.java
@@ -17,8 +17,4 @@ public class ProfileChangeRequestDto {
     @Email(message = "이메일 형식이 아닙니다.")
     @Size(min = 1, max = 30, message = "이메일은 최소 1자 최대 30자로 설정해야됩니다.")
     private String email;
-
-    @NotBlank
-    @Size(min = 1, max = 100, message = "비밀번호는 최소 1자 최대 100자로 설정해야됩니다.")
-    private String password;
 }
