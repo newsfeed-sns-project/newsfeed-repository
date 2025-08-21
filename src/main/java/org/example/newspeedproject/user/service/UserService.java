11802a7ab6ce798f8ea3a0b628841c6049ce15c4
doldollee00 <50581232+doldollee00@users.noreply.github.com>
2025-08-21 17:13:32 +0900

    Update UserService.java
    User 예외처리 수정

--- src/main/java/org/example/newspeedproject/user/service/UserService.java
+++ src/main/java/org/example/newspeedproject/user/service/UserService.java
@@ -20,13 +20,13 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public UserResponseDto findUserId(Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다."));
+        User user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("유저가 존재하지 않습니다."));
         return new UserResponseDto(user.getId(), user.getUsername(), user.getEmail(), user.getCreatedAt(), user.getModifiedAt());
     }
 
     @Transactional
     public UserResponseDto updateUserProfile(Long id, ProfileChangeRequestDto request) {
-        User user = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다."));
+        User user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("유저가 존재하지 않습니다."));
 
         user.updateUser(request.getUsername(), request.getEmail());
         return new UserResponseDto(user.getId(), user.getUsername(), user.getEmail(), user.getCreatedAt(), user.getModifiedAt());
@@ -34,14 +34,14 @@ public class UserService {
 
     @Transactional
     public void passwordChange(Long id, PasswordChangeRequestDto request) {
-        User user = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다."));
+        User user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("유저가 존재하지 않습니다."));
 
         if(!passwordEncoder.matches(request.getOldpassword(), user.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
         if(passwordEncoder.matches(request.getNewpassword(), user.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "변경될 비밀번호가 이전 비밀번호와 동일합니다.");
+            throw new IllegalArgumentException("변경될 비밀번호가 이전 비밀번호와 동일합니다.");
         }
 
         String encodedPassword = passwordEncoder.encode(request.getNewpassword());
@@ -50,14 +50,14 @@ public class UserService {
 
     @Transactional
     public void deleteUser(Long id, LoginRequestDto request) {
-        User user = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다."));
+        User user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("유저가 존재하지 않습니다."));
 
         if (!user.getEmail().equals(request.getEmail())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이메일이 일치하지 않습니다.");
+            throw new IllegalArgumentException("이메일이 일치하지 않습니다.");
         }
 
         if(!passwordEncoder.matches(request.getPassword(),  user.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
         userRepository.delete(user);
